1c4a4e208e85d1d368e02c5f0c8cf89ccda751a5
Sarath Sajan <sarathsajan1998@gmail.com>
2020-03-27 21:05:26 +0530

    added/uploaded all the files to Github

---
+++ applcs.py
@@ -0,0 +1,220 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from functools import wraps
+from data import Songs
+import random
+import time
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'addyoursecretkeyhere'	#this method for adding session secret key in pythonanywhere
+
+songs_data = Songs()
+
+def is_registered(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'registered' in session:
+			return f(*args, **kwargs)
+		else:
+			return redirect(url_for('register'))
+	return wrap
+
+
+@app.route('/')
+def index():
+	return render_template('home.html')
+
+@app.route('/about/')
+def about():
+	return render_template('about.html')
+
+
+
+##################Core game logic and random number generation
+@app.route('/random_security/')
+@is_registered
+def random_security():
+	if True:
+		#time.sleep(1)
+		#session['rand_num'] = random.randint(0, len(songs_data)-1)
+		session['rand_num'] = random.choice(session['unvisited'])
+		session['unvisited'].remove(session['rand_num'])
+		if len(session['unvisited']) < 1:
+			session['gamecomplete'] = True
+			return redirect(url_for('gamecomplete'))
+		return redirect(url_for('game'))
+	return('Generating Random Key...')
+
+
+class SongForm(Form):
+	name = StringField('Identify the song<br>(NOTE: select from the drop-down as you type)', [validators.DataRequired()])
+
+@app.route('/game/', methods = ['GET', 'POST'])
+@is_registered
+def game():
+	#rand_num = random.randint(0, 4)
+	form = SongForm(request.form)
+	if request.method == 'POST' and form.validate():
+		session['name_candidate'] = form.name.data
+		return redirect(url_for('score'))
+	return render_template('game.html', i = session['rand_num'], songs = songs_data, form = form, score = session['score'])
+
+################# giving clues
+@app.route('/gameclue', methods = ['GET', 'POST'])
+@is_registered
+def clue():
+	session['clue'] = True
+	form = SongForm(request.form)
+	if request.method == 'POST' and form.validate():
+		session['name_candidate'] = form.name.data
+		return redirect(url_for('score'))
+	return render_template('gameclue.html', i = session['rand_num'], songs = songs_data, form = form, score = session['score'])
+
+################# music scoring engine
+
+#this function is useed as udayip spell checker, not at all efficient
+def LongestCommonSubsequence(X, Y):
+    # find the length of the strings 
+    m = len(X) 
+    n = len(Y) 
+  
+    # declaring the array for storing the dp values 
+    L = [[None]*(n+1) for i in xrange(m+1)] 
+  
+    """Following steps build L[m+1][n+1] in bottom up fashion 
+    Note: L[i][j] contains length of LCS of X[0..i-1] 
+    and Y[0..j-1]"""
+    for i in range(m+1): 
+        for j in range(n+1): 
+            if i == 0 or j == 0 : 
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                L[i][j] = L[i-1][j-1]+1
+            else: 
+                L[i][j] = max(L[i-1][j] , L[i][j-1]) 
+  
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1] 
+    return L[m][n]
+
+
+@app.route('/muscorengine')
+@is_registered
+def score():
+	song_answer = songs_data[session['rand_num']]
+
+	closeness = LongestCommonSubsequence(session['name_candidate'].lower(), song_answer['name'].lower())
+	closenesspercent = (closeness*100.0)/len(song_answer['name'])
+	print "\n\n\n\n\n\n\n\n closeness percent -->", closenesspercent
+
+	if closenesspercent >= 70.0:
+	#if session['name_candidate'].lower() == song_answer['name'].lower():
+		if session['clue'] == True:
+			session['clue'] = False
+			flash('You don\'t get any points since you used clues', 'danger')
+			return redirect(url_for('random_security'))
+		else:
+			session['score'] = session['score'] + 1
+			return redirect(url_for('random_security'))
+	else:
+		return redirect(url_for('gameover'))
+	return ('Checking answer...')
+
+################# registration and authorisation
+class RegisterForm(Form):
+	name = StringField('Your name', [validators.Length(min = 3, max = 50), validators.DataRequired()])
+
+@app.route('/register/', methods = ['GET', 'POST'])
+def register():
+	session.clear()
+	form = RegisterForm(request.form)
+	if request.method == 'POST' and form.validate():
+		name = form.name.data
+		session['registered'] = True
+		session['score'] = 0
+		session['clue'] = False
+		session['gamecomplete'] = False
+
+		session['unvisited'] = []
+		for i in range(0, len(songs_data)):
+			print "/n/n/n/n/n",i
+			session['unvisited'].append(i)
+
+		session['username'] = name 							#succesfully entered the name, now you can play
+		flash("Let's start, {}".format(session['username']), 'success')
+		return redirect(url_for('random_security'))
+	return render_template('register.html', form = form)
+
+@app.route('/thaan_oru_potanaa')
+@is_registered
+def redhand():
+	return('Njan oru pottan annenu karithiyo ?')
+
+def palindrome(n):
+	temp=n
+	rev=0
+	while(n>0):
+	    dig=n%10
+	    rev=rev*10+dig
+	    n=n//10
+	if(temp==rev):
+	    return 1
+	else:
+	    return 0
+
+################# quiting the game or Getting game-over-ed
+@app.route('/gameover')
+@is_registered
+def gameover():
+	final_score = session['score']
+	final_rand_num = session['rand_num']
+	song_name = songs_data[session['rand_num']]['name']
+	film_name = songs_data[session['rand_num']]['film']
+	release_year = songs_data[session['rand_num']]['year']
+	yturl = songs_data[session['rand_num']]['yturl']
+	flag = 0
+	flag = palindrome(final_score)
+	
+	if flag == 1:
+		category = 'palindrome'
+		reaction = 1
+
+	if (final_score)*(100)/(len(songs_data)) <= 10 and flag == 1:
+		category = 2
+		reaction = random.randint(1, 13)
+	if (final_score)*(100)/(len(songs_data)) > 10 and final_score <= 20 and flag == 0:
+		category = 4
+		reaction = random.randint(1, 9)
+	if (final_score)*(100)/(len(songs_data)) > 20 and final_score <= 30 and flag == 0:
+		category = 8
+		reaction = random.randint(1, 14)
+	if (final_score)*(100)/(len(songs_data)) > 30 and final_score <= 40 and flag == 0:
+		category = 16
+		reaction = random.randint(1, 10)
+	if (final_score)*(100)/(len(songs_data)) > 40 and final_score <= 50 and flag == 0:
+		category = 32
+		reaction = random.randint(1, 5)
+	if (final_score)*(100)/(len(songs_data)) > 50 and final_score <= 60 and flag == 0:
+		category = 64
+		reaction = random.randint(1, 8)
+	if (final_score)*(100)/(len(songs_data)) > 60 and final_score < 70 and flag == 0:
+		category = 128
+		reaction = random.randint(1, 6)
+	if (final_score)*(100)/(len(songs_data)) >= 70 and flag == 0:
+		category = 256
+		reaction = random.randint(1, 7)
+	session.clear()
+	return render_template('gameover.html', song_name = song_name, film_name = film_name, release_year = release_year, yturl = yturl, final_score = final_score, category = category, reaction = reaction)
+
+################# When the user is lit af and completes the effing game. Shit
+@app.route('/gamecomplete')
+@is_registered
+def gamecomplete():
+	if session['gamecomplete'] == True:
+		session.clear()
+		return render_template('gamecomplete.html')
+	else:
+		return redirect(url_for('redhand'))
+
+if __name__ == "__main__":
+	#app.secret_key = "addyoursecretkeyhere"     #how to add secret key in python anywhere
+	app.run(debug = True)
---
+++ data.py
@@ -0,0 +1,431 @@
+def Songs():
+	songs = [
+				{
+					'name'	:	'Entammede Jimikki Kammal',
+					'film'	:	'Velipadinte Pusthakam',
+					'year'	:	'2017',
+					'yturl'	:	'https://www.youtube.com/watch?v=FXiaIH49oAU',
+					'slno'	:	0
+				},
+				{
+					'name'	:	'Karakana Kadalala Mele',
+					'film'	:	'Nadodikkattu',
+					'year'	:	'1987',
+					'yturl'	:	'https://www.youtube.com/watch?v=NWZ_Ull4KbQ',
+					'slno'	:	1
+				},
+				{
+					'name'	:	'Lajjavathiye',
+					'film'	:	'4 the People',
+					'year'	:	'2004',
+					'yturl'	:	'https://www.youtube.com/watch?v=aJXI1zDdZvM',
+					'slno'	:	2
+				},
+				{
+					'name'	:	'Unnam Marannu',
+					'film'	:	'In Harihar Nagar',
+					'year'	:	'1990',
+					'yturl'	:	'https://www.youtube.com/watch?v=n78X2grt7CM',
+					'slno'	:	3
+				},
+				{
+					'name'	:	'Padakali',
+					'film'	:	'Yodha',
+					'year'	:	'1992',
+					'yturl'	:	'https://www.youtube.com/watch?v=I5mfYkhycP0',
+					'slno'	:	4
+				},
+				{
+					'name'	:	'Olathumbathirunnooyaladum',
+					'film'	:	'Pappayude Swantham Appoos',
+					'year'	:	'1992',
+					'yturl'	:	'https://www.youtube.com/watch?v=ELEaxpW1JPM',
+					'slno'	:	5
+				},
+				{
+					'name'	:	'Velukkumbol Kulikkuvan',
+					'film'	:	'Kuttikkuppayam',
+					'year'	:	'1964',
+					'yturl'	:	'https://www.youtube.com/watch?v=aZvXSL-7GCc',
+					'slno'	:	6
+				},
+				{
+					'name'	:	'Pennale Pennale',
+					'film'	:	'Chemmeen',
+					'year'	:	'1965',
+					'yturl'	:	'https://www.youtube.com/watch?v=sTY-Xzd5EiA',
+					'slno'	:	7
+				},
+				{
+					'name'	:	'Ellarum Chollanu',
+					'film'	:	'Neelakuyil',
+					'year'	:	'1954',
+					'yturl'	:	'https://www.youtube.com/watch?v=XNBwwiNgQq8',
+					'slno'	:	8
+				},
+				{
+					'name'	:	'Kaanana Chaayayil',
+					'film'	:	'Ramanan',
+					'year'	:	'1967',
+					'yturl'	:	'https://www.youtube.com/watch?v=_qyzqGuzE9I',
+					'slno'	:	9
+				},
+				{
+					'name'	:	'Ninte Mizhiyil',
+					'film'	:	'Arakkallan Mukkaal Kallan',
+					'year'	:	'1974',
+					'yturl'	:	'https://www.youtube.com/watch?v=Odh59GuyR5k',
+					'slno'	:	10
+				},
+				{
+					'name'	:	'Illavanoor Madathile',
+					'film'	:	'Kadathanaattu Maakkam',
+					'year'	:	'1978',
+					'yturl'	:	'https://www.youtube.com/watch?v=_j9B30vIJxc',
+					'slno'	:	11
+				},
+				{
+					'name'	:	'Ponnalayil Ammanamadi',
+					'film'	:	'Devadasi',
+					'year'	:	'1979',
+					'yturl'	:	'https://www.youtube.com/watch?v=hSuloOju89M',
+					'slno'	:	12
+				},
+				{
+					'name'	:	'Oru Vattam Koodiyen',
+					'film'	:	'Chillu',
+					'year'	:	'1982',
+					'yturl'	:	'https://www.youtube.com/watch?v=bFk6fObx3qQ',
+					'slno'	:	13
+				},
+				{
+					'name'	:	'Ilam Manjin',
+					'film'	:	'Ninnishtam Ennishtam',
+					'year'	:	'1986',
+					'yturl'	:	'https://www.youtube.com/watch?v=5m2o1LnJkTM',
+					'slno'	:	14
+				},
+				{
+					'name'	:	'Paadam Pootha Kaalam',
+					'film'	:	'Chithram',
+					'year'	:	'1988',
+					'yturl'	:	'https://www.youtube.com/watch?v=2H91cj62BXE',
+					'slno'	:	15
+				},
+				{
+					'name'	:	'Paalappoove',
+					'film'	:	'Njan Gandharvan',
+					'year'	:	'1991',
+					'yturl'	:	'https://www.youtube.com/watch?v=hMm1-iuYUKE',
+					'slno'	:	16
+				},
+				{
+					'name'	:	'Mele Mele Maanam',
+					'film'	:	'No 1 Snehatheeram Bangalore North',
+					'year'	:	'1995',
+					'yturl'	:	'https://www.youtube.com/watch?v=vj7gM-XliIg',
+					'slno'	:	17
+				},
+				{
+					'name'	:	'Oru Raajamalli',
+					'film'	:	'Aniyathipraavu',
+					'year'	:	'1997',
+					'yturl'	:	'https://www.youtube.com/watch?v=biLUmiD4oUo',
+					'slno'	:	18
+				},
+				{
+					'name'	:	'Kanneer Mazhayathu',
+					'film'	:	'Joker',
+					'year'	:	'2000',
+					'yturl'	:	'https://www.youtube.com/watch?v=TuuB3kdxSlk',
+					'slno'	:	19
+				},
+				{
+					'name'	:	'Ariyathe Ariyathe',
+					'film'	:	'Ravanaprabhu',
+					'year'	:	'2001',
+					'yturl'	:	'https://www.youtube.com/watch?v=tzeRfnQvPEU',
+					'slno'	:	20
+				},
+				{
+					'name'	:	'Vavavo Vave',
+					'film'	:	'Ente Veedu Appuvinteyum',
+					'year'	:	'2003',
+					'yturl'	:	'https://www.youtube.com/watch?v=SvBNaBAqaEs',
+					'slno'	:	21
+				},
+				{
+					'name'	:	'Veshangal Janmangal',
+					'film'	:	'Vesham',
+					'year'	:	'2004',
+					'yturl'	:	'https://www.youtube.com/watch?v=Yv6ABNK7kyE',
+					'slno'	:	22
+				},
+				{
+					'name'	:	'Nillu Nillu',
+					'film'	:	'Rain Rain Come Again',
+					'year'	:	'2004',
+					'yturl'	:	'https://www.youtube.com/watch?v=RBHDWZrnrPA',
+					'slno'	:	23
+				},
+				{
+					'name'	:	'Kai Niraye',
+					'film'	:	'Baba Kalyani',
+					'year'	:	'2006',
+					'yturl'	:	'https://www.youtube.com/watch?v=jlnGrfWRANk',
+					'slno'	:	24
+				},
+				{
+					'name'	:	'Vottu',
+					'film'	:	'Classmates',
+					'year'	:	'2006',
+					'yturl'	:	'https://www.youtube.com/watch?v=sOIpuW8HjAc',
+					'slno'	:	25
+				},
+				{
+					'name'	:	'Thirike njan varumenna',
+					'film'	:	'Arabhikatha',
+					'year'	:	'2007',
+					'yturl'	:	'https://www.youtube.com/watch?v=W8seNXtIx0g',
+					'slno'	:	26
+				},
+				{
+					'name'	:	'Enkaralil',
+					'film'	:	'Nammal',
+					'year'	:	'2002',
+					'yturl'	:	'https://www.youtube.com/watch?v=j9P_-dFNkwg',
+					'slno'	:	27
+				},
+				{
+					'name'	:	'Azhalinte Azhangalil',
+					'film'	:	'Ayalum Njanum Thammil',
+					'year'	:	'2012',
+					'yturl'	:	'https://www.youtube.com/watch?v=mSzrX7YWdKg',
+					'slno'	:	28
+				},
+				{
+					'name'	:	'Nilamalare',
+					'film'	:	'Diamond Necklace',
+					'year'	:	'2012',
+					'yturl'	:	'https://www.youtube.com/watch?v=sgD7Y9ZylAA',
+					'slno'	:	29
+				},
+				{
+					'name'	:	'Jeevamshamayi',
+					'film'	:	'Theevandi',
+					'year'	:	'2018',
+					'yturl'	:	'https://www.youtube.com/watch?v=DInfmi-YIiw',
+					'slno'	:	30
+				},
+				{
+					'name'	:	'Pinneyum Pinneyum',
+					'film'	:	'Krishnagudiyil Oru Pranayakalathu',
+					'year'	:	'1997',
+					'yturl'	:	'https://www.youtube.com/watch?v=v1tfC5mg6iE',
+					'slno'	:	31
+				},
+				{
+					'name'	:	'Vel Muruga',
+					'film'	:	'Naran',
+					'year'	:	'2005',
+					'yturl'	:	'https://www.youtube.com/watch?v=FUcAN76vLEk',
+					'slno'	:	32
+				},
+				{
+					'name'	:	'Oru Nokku Kaanuvan',
+					'film'	:	'Sunday Holiday',
+					'year'	:	'2017',
+					'yturl'	:	'https://www.youtube.com/watch?v=mMMEp6lLj8E',
+					'slno'	:	33
+				},
+				{
+					'name'	:	'Mizhi Ariyathe',
+					'film'	:	'Niram',
+					'year'	:	'1999',
+					'yturl'	:	'https://www.youtube.com/watch?v=J6_2zEYa2dQ',
+					'slno'	:	34
+				},
+				{
+					'name'	:	'Harimuraleeravam',
+					'film'	:	'Aaraam Thampuran',
+					'year'	:	'1997',
+					'yturl'	:	'https://www.youtube.com/watch?v=TCi7Nwb0RyI',
+					'slno'	:	35
+				},
+				{
+					'name'	:	'Karutha Penne',
+					'film'	:	'Thenmavin Kombath',
+					'year'	:	'1994',
+					'yturl'	:	'https://www.youtube.com/watch?v=vZSzvGpDrj0',
+					'slno'	:	36
+				},
+				{
+					'name'	:	'Confusion Theerkaname',
+					'film'	:	'Summer In Bethlehem',
+					'year'	:	'1998',
+					'yturl'	:	'https://www.youtube.com/watch?v=GDM4gzGZHQ4',
+					'slno'	:	37
+				},
+				{
+					'name'	:	'Manathe Chandiranothoru',
+					'film'	:	'Chandralekha',
+					'year'	:	'1997',
+					'yturl'	:	'https://www.youtube.com/watch?v=BcDBV4ef7to',
+					'slno'	:	38
+				},
+				{
+					'name'	:	'Pathinalam Ravinte Pira Pole',
+					'film'	:	'Sharjah To Sharjah',
+					'year'	:	'2001',
+					'yturl'	:	'https://www.youtube.com/watch?v=4FrJxhF56ag',
+					'slno'	:	39
+				},
+				{
+					'name'	:	'Kannadi Koodum Kootti',
+					'film'	:	'Pranayavarnangal',
+					'year'	:	'1998',
+					'yturl'	:	'https://www.youtube.com/watch?v=Fgu4YYnjXio',
+					'slno'	:	40
+				},
+				{
+					'name'	:	'Aaro Viral Meeti',
+					'film'	:	'Pranayavarnangal',
+					'year'	:	'1998',
+					'yturl'	:	'https://www.youtube.com/watch?v=nUc2X4x55CE',
+					'slno'	:	41
+				},
+				{
+					'name'	:	'Omanapuzha',
+					'film'	:	'Chandupottu',
+					'year'	:	'2005',
+					'yturl'	:	'https://www.youtube.com/watch?v=TGReArJqs88',
+					'slno'	:	42
+				},
+				{
+					'name'	:	'Manju Peyyanu',
+					'film'	:	'Chandranudikkunnadhikkil',
+					'year'	:	'1999',
+					'yturl'	:	'https://www.youtube.com/watch?v=69TAUeBL_gE',
+					'slno'	:	43
+				},
+				{
+					'name'	:	'Kadhayile Rajakumaariyum',
+					'film'	:	'Kalyanaraman',
+					'year'	:	'2002',
+					'yturl'	:	'https://www.youtube.com/watch?v=sqKpwZgRflc',
+					'slno'	:	44
+				},
+				{
+					'name'	:	'Doore Kizhakkudikkum',
+					'film'	:	'Chithram',
+					'year'	:	'1988',
+					'yturl'	:	'https://www.youtube.com/watch?v=bGuX3RHLI2Q',
+					'slno'	:	45
+				},
+				{
+					'name'	:	'Enne Thallendammaava',
+					'film'	:	'Oru Vadakkan Selfie',
+					'year'	:	'2015',
+					'yturl'	:	'https://www.youtube.com/watch?v=-r6HmTK9l9g',
+					'slno'	:	46
+				},
+				{
+					'name'	:	'Nethavu',
+					'film'	:	'Oru Indian Pranayakatha',
+					'year'	:	'2013',
+					'yturl'	:	'https://www.youtube.com/watch?v=-kCb1uTgp2I',
+					'slno'	:	47
+				},
+				{
+					'name'	:	'Karu Karu Karuthoru',
+					'film'	:	'Balettan',
+					'year'	:	'2003',
+					'yturl'	:	'https://www.youtube.com/watch?v=W7o1nZTdwmg',
+					'slno'	:	48
+				},
+				{
+					'name'	:	'Chandanamani',
+					'film'	:	'Praja',
+					'year'	:	'2001',
+					'yturl'	:	'https://www.youtube.com/watch?v=C-lHPG3sWHw',
+					'slno'	:	49
+				},
+				{
+					'name'	:	'Anuraagathin Velayil',
+					'film'	:	'Thattathin Marayathu',
+					'year'	:	'2012',
+					'yturl'	:	'https://www.youtube.com/watch?v=a1tZ89zCdxs',
+					'slno'	:	50
+				},
+				{
+					'name'	:	'Innale Ente Nenjile',
+					'film'	:	'Balettan',
+					'year'	:	'2003',
+					'yturl'	:	'https://www.youtube.com/watch?v=6IIz1Adn_7E',
+					'slno'	:	51
+				},
+				{
+					'name'	:	'Karale Karalinte',
+					'film'	:	'Udayananu Tharam',
+					'year'	:	'2005',
+					'yturl'	:	'https://www.youtube.com/watch?v=0lxQb7wx1zU',
+					'slno'	:	52
+				},
+				{
+					'name'	:	'Karuppinazhaku',
+					'film'	:	'Swapnakoodu',
+					'year'	:	'2003',
+					'yturl'	:	'https://www.youtube.com/watch?v=YHi0qlMIEKE',
+					'slno'	:	53
+				},
+				{
+					'name'	:	'Chundathu Chethipoo',
+					'film'	:	'Chronic Bachelor',
+					'year'	:	'2003',
+					'yturl'	:	'https://www.youtube.com/watch?v=vBCb91BWcmo',
+					'slno'	:	54
+				},
+				{
+					'name'	:	'Khudase',
+					'film'	:	'Keerthi Chakra',
+					'year'	:	'2006',
+					'yturl'	:	'https://www.youtube.com/watch?v=NZiG1SR_WhA',
+					'slno'	:	55
+				},
+				{
+					'name'	:	'Alliyambal Kadavil',
+					'film'	:	'Rosie',
+					'year'	:	'1965',
+					'yturl'	:	'https://www.youtube.com/watch?v=G0PvfL1Lku0',
+					'slno'	:	56
+				},
+				{
+					'name'	:	'Kasthoori Manmizhi',
+					'film'	:	'Manushya Mrugam',
+					'year'	:	'1980',
+					'yturl'	:	'https://www.youtube.com/watch?v=yHzbiwJIAzw',
+					'slno'	:	57
+				},
+				{
+					'name'	:	'Maada Praave Vaa',
+					'film'	:	'Madanolsavam',
+					'year'	:	'1977',
+					'yturl'	:	'https://www.youtube.com/watch?v=36-2G5arjg0',
+					'slno'	:	58
+				},
+				{
+					'name'	:	'Thenum Vayambum',
+					'film'	:	'Thenum Vayambum',
+					'year'	:	'1981',
+					'yturl'	:	'https://www.youtube.com/watch?v=8q6FM63e_O8',
+					'slno'	:	59
+				},
+				{
+					'name'	:	'Unarumee Gaanam',
+					'film'	:	'Moonam Pakkam',
+					'year'	:	'1988',
+					'yturl'	:	'https://www.youtube.com/watch?v=aBXab6VtX5w',
+					'slno'	:	60
+				}
+			]
+	return songs
